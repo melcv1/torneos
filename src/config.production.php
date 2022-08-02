a6bb5cc92f98146d5b38679ff13856342c386a95
Melissa <lmcedeno2@espe.edu.ec>
2022-08-02 14:45:13 -0500

    h

--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "uC9sZp3phkVUyXxd", // API Secret Key
+        "SECRET_KEY" => "tY5TRhv37EzhuUrj", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
